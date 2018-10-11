58356e7f4e6d7a061e391964ca2f39ba6f1c4334
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-10-12 10:23:42 +1100

    x

--- pproc/yfcc100m/quantify_broken_image.py
+++ pproc/yfcc100m/quantify_broken_image.py
@@ -54,8 +54,8 @@ def main():
           tag_count[tag] = tag_count.get(tag, 0) + 1
 
       t_line += 1
-      if t_line == 50000:
-        break
+      # if t_line == 50000:
+      #   break
       if (t_line % 5000000) == 0:
         print('line#%09d' % (t_line))
   print('%s contains %d lines in total' % (dataset_file, t_line))
