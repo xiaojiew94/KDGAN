a6a46222457026366da66869be5287617cf216b1
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-10-11 15:08:15 +1100

    x

--- pproc/yfcc100m/quantify_broken_image.py
+++ pproc/yfcc100m/quantify_broken_image.py
@@ -7,6 +7,9 @@ def main():
   with open(dataset_infile) as fin:
     for line in fin.readlines():
       t_line += 1
+
+      if (t_line % 10000) == 0:
+        print('line#%07d' % (t_line))
   print('%s contains %d lines in total' % (dataset_infile, t_line))
 
 if __name__ == '__main__':
