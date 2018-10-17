b7ed4f9eb8157dba6f0f26658a5612cbb18a90d8
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-10-18 09:52:11 +1100

    x

--- pproc/yfcc100m/quantify_broken_image.py
+++ pproc/yfcc100m/quantify_broken_image.py
@@ -20,7 +20,7 @@ sep_word = '+'
 
 def main():
   unamb_nouns = pickle.load(open('imagenet_tag_set.p', 'rb'))
-  unamb_nouns = pickle.load(open('wordnet_tag_set.p', 'rb'))
+  # unamb_nouns = pickle.load(open('wordnet_tag_set.p', 'rb'))
 
   num_tag = 0
   tags = []
@@ -34,8 +34,8 @@ def main():
       tag, count = fields[0], int(fields[1])
 
       valid = True
-      if count < 200:
-        valid = False
+      # if count < 200:
+      #   valid = False
       if not tag in unamb_nouns:
         valid = False
       if valid:
