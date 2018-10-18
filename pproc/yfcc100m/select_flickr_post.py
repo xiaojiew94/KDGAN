1342900d6c0195635b9384766400707afffa5928
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-10-18 11:21:28 +1100

    x

--- pproc/yfcc100m/select_flickr_post.py
+++ pproc/yfcc100m/select_flickr_post.py
@@ -10,7 +10,8 @@ in_file_p = 'imagenet_tag_set.p'
 wn_file_p = 'wordnet_tag_set.p'
 
 num_field = 25
-idx_field = 10
+idx_user = 3
+idx_tag = 10
 idx_marker = 24
 sep_field = '\t'
 sep_tag = ','
@@ -20,24 +21,33 @@ min_in_tag = 20
 min_wn_tag = 100
 
 def get_tag_count(in_tag_set, wn_tag_set):
+  user_count = {}
+  tot_line = 0
   with open(dataset_file) as fin:
     while True:
       line = fin.readline()
       if not line:
         break
+      tot_line += 1
+      if (tot_line % 5000000) == 0:
+        print('line#%09d' % (tot_line))
+
       fields = line.strip().split(sep_field)
       assert len(fields) == num_field
       if fields[idx_marker] != '0': # not image
         continue
+      if len(fields[idx_tag]) == 0: # no tags
+        continue
       is_valid = False
-      tags = fields[idx_field].split(sep_tag)
-      print(tags)
-      input()
+      tags = fields[idx_tag].split(sep_tag)
       for tag in tags:
         if tag in in_tag_set or tag in wn_tag_set:
           is_valid = True
           break
-
+      user = fields[idx_user]
+      print(user)
+      input()
+      user_count[user] = user_count.get(user, 0) + 1
 
   in_tag_count, wn_tag_count = {}, {}
   return in_tag_count, wn_tag_count
