55bcbe7642f9779a117ff8dcf255c5f7ce99c1c1
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-10-18 17:08:48 +1100

    x

--- pproc/yfcc100m/miscellaneous_test.py
+++ pproc/yfcc100m/miscellaneous_test.py
@@ -1,38 +1,17 @@
 from operator import itemgetter
 from urllib import request
 
-
 def main():
-  base_url = 'https://raw.githubusercontent.com/tensorflow/models/master/research/inception/inception/data/'
-  synset_url = '{}/imagenet_lsvrc_2015_synsets.txt'.format(base_url)
-  synset_to_human_url = '{}/imagenet_metadata.txt'.format(base_url)
+  file = '/home/xiaojie/Projects/data/yfcc100m/yfcc_top/yfcc10k.data'
+  with open(file) as fin:
+    while True:
+      line = fin.readline()
+      if not line:
+        break
+      fields = line.strip().split('\t')
 
-  filename, _ = request.urlretrieve(synset_url)
-  synset_list = [s.strip() for s in open(filename).readlines()]
-  num_synsets_in_ilsvrc = len(synset_list)
-  assert num_synsets_in_ilsvrc == 1000
-
-  filename, _ = request.urlretrieve(synset_to_human_url)
-  synset_to_human_list = open(filename).readlines()
-  num_synsets_in_all_imagenet = len(synset_to_human_list)
-  assert num_synsets_in_all_imagenet == 21842
-
-  synset_to_human = {}
-  for s in synset_to_human_list:
-    parts = s.strip().split('\t')
-    assert len(parts) == 2
-    synset = parts[0]
-    human = parts[1]
-    synset_to_human[synset] = human
-
-  label_index = 1
-  labels_to_names = {0: 'background'}
-  for synset in synset_list:
-    name = synset_to_human[synset]
-    labels_to_names[label_index] = name
-    label_index += 1
-
-  print(labels_to_names)
+      print(fields)
+      input()
 
 def test():
   file = '/home/xiaojie/Projects/data/yfcc100m/yfcc_rnd/yfcc_rnd.data'
