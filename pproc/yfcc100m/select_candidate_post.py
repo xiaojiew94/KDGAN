8cbb372caf7df08ec02b3bd941ad91d2cdf93603
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-10-19 17:14:12 +1100

    x

--- pproc/yfcc100m/select_candidate_post.py
+++ pproc/yfcc100m/select_candidate_post.py
@@ -11,21 +11,20 @@ dataset_file = '/data/yfcc100m/yfcc100m_dataset'
 in_tag_count_file = path.join(data_dir, 'imagenet_tag_count.p')
 wn_tag_count_file = path.join(data_dir, 'wordnet_tag_count.p')
 user_count_file = path.join(data_dir, 'flickr_user_count.p')
-def main():
-  in_tag_count = pickle.load(open(in_tag_count_file, 'rb'))
-  wn_tag_count = pickle.load(open(wn_tag_count_file, 'rb'))
-  user_count = pickle.load(open(user_count_file, 'rb'))
 
+def get_count(in_tag_count, wn_tag_count, user_count):
   in_tag_set = set(in_tag_count.keys())
   wn_tag_set = set(wn_tag_count.keys())
   user_set = set(user_count.keys())
   num_in_tag = len(in_tag_set)
   num_wn_tag = len(wn_tag_set)
   num_user = len(user_set)
-  print('#imagenet=%d #wordnet=%d' % (num_in_tag, num_wn_tag))
-  print('#user=%d' % (num_user))
+  logging.info('#imagenet=%d #wordnet=%d' % (num_in_tag, num_wn_tag))
+  logging.info('#user=%d' % (num_user))
 
-  num_valid = 0
+  in_tag_count, wn_tag_count, user_count = {}, {}, {}
+
+  num_post = 0
   tot_line = 0
   with open(dataset_file) as fin:
     while True:
@@ -34,7 +33,7 @@ def main():
         break
       tot_line += 1
       if (tot_line % 20000000) == 0:
-        logging.info('line#%09d %d' % (tot_line, num_valid))
+        logging.info('line#%09d %d' % (tot_line, num_post))
 
       fields = line.strip().split(sep_field)
       assert len(fields) == num_field
@@ -45,11 +44,13 @@ def main():
       user = fields[idx_user]
       if user not in user_set:
         continue
-      tags = []
+      in_tags, wn_tags = [], []
       for tag in fields[idx_tag].split(sep_tag):
-        if tag in in_tag_set or tag in wn_tag_set:
-          tags.append(tag)
-      if len(tags) == 0:
+        if tag in in_tag_set:
+          in_tags.append(tag)
+        if tag in wn_tag_set:
+          wn_tags.append(tag)
+      if len(in_tags) + len(wn_tags) == 0:
         continue
       title = fields[idx_title]
       description = fields[idx_description]
@@ -60,8 +61,32 @@ def main():
           break
       if not is_valid:
         continue
-      num_valid += 1
-  logging.info('#valid=%d' % (num_valid))
+      num_post += 1
+      for tag in in_tags:
+        in_tag_count[tag] = in_tag_count.get(tag, 0) + 1
+      for tag in wn_tags:
+        wn_tag_count[tag] = wn_tag_count.get(tag, 0) + 1
+      user_count[user] = user_count.get(tag, 0) + 1
+  num_user = len(user_count)
+  num_in_tag = len(in_tag_count)
+  num_wn_tag = len(wn_tag_count)
+  logging.info('#user=%d #post=%d' % (num_user, num_post))
+  logging.info('#imagenet=%d #wordnet=%d' % (num_in_tag, num_wn_tag))
+  return in_tag_count, wn_tag_count, user_count
+
+def main():
+  in_tag_count = pickle.load(open(in_tag_count_file, 'rb'))
+  wn_tag_count = pickle.load(open(wn_tag_count_file, 'rb'))
+  user_count = pickle.load(open(user_count_file, 'rb'))
+
+  while True:
+    results = get_count(in_tag_count, wn_tag_count, user_count)
+    in_tag_count, wn_tag_count, user_count = results
+
+    if (min(in_tag_count.values()) >= min_in_tag and
+        min(wn_tag_count.values()) >= min_wn_tag and
+        min(user_count.values()) >= min_user):
+      break
 
 if __name__ == '__main__':
   main()
