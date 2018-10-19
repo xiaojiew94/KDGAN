ae5c55b5c5879eea3469f631aea10eaeed76948e
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-10-20 10:46:12 +1100

    x

--- pproc/yfcc100m/main.sh
+++ pproc/yfcc100m/main.sh
@@ -1,5 +1,5 @@
 python -W ignore select_imagenet_tag.py
 python -W ignore select_wordnet_tag.py
 
-python -W ignore select_candidate_tag.py -o
-python -W ignore select_candidate_post.py -o
+python -W ignore filter_post_by_noun.py -o
+python -W ignore filter_post_by_text.py -o
