a44f52c36d0f4d59b132cb8c1c585e5b03f221a7
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-10-20 13:46:29 +1100

    x

--- pproc/yfcc100m/split_image_url.py
+++ pproc/yfcc100m/split_image_url.py
@@ -1,11 +1,12 @@
 from utils import *
 
+import argparse
 import pickle
 
 import logging
 logging.basicConfig(level=logging.INFO, format=log_format)
 
-def main():
+def main(num_fold):
   tot_line = 0
   with open(yfcc_sample_file) as fin:
     while True:
@@ -16,6 +17,11 @@ def main():
       if (tot_line % 200000) == 0:
         logging.info('#line=%07d' % (tot_line))
   logging.info('#line=%7d' % (tot_line))
+  print(num_fold)
+
 
 if __name__ == '__main__':
-  main()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('num_fold', type=int)
+  args = parser.parse_args()
+  main(args.num_fold)
