af287e1e99c8f504ffcee6cc2270e290b02cec50
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-10-20 14:05:01 +1100

    x

--- pproc/yfcc100m/utils.py
+++ pproc/yfcc100m/utils.py
@@ -2,6 +2,7 @@ from os import path
 
 yfcc_dataset_file = '/data/yfcc100m/yfcc100m_dataset'
 yfcc_sample_file = '/data/yfcc100m/yfcc100m_sample'
+flickr_image_dir = '/data/yfcc100m/flickr_images'
 
 data_dir = 'data'
 in_all_noun_rfile = path.join(data_dir, 'in_all_noun.r')
@@ -14,8 +15,6 @@ initial_user_pfile = path.join(data_dir, 'initial_user.p')
 in_refined_noun_pfile = path.join(data_dir, 'in_refined_noun.p')
 wn_refined_noun_pfile = path.join(data_dir, 'wn_refined_noun.p')
 refined_user_pfile = path.join(data_dir, 'refined_user.p')
-
-flickr_image_dir = 'flickr_images'
 
 min_user = 20 # 40
 min_in_tag = 20 # 40
